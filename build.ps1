52a007712e33430663dfe0d58db1f1d82eefe8ac
mochensky <aipatov5127@gmail.com>
2026-04-24 17:02:22 +0300

    rename: main -> max2tg-amd64

--- build.ps1
+++ build.ps1
@@ -7,10 +7,10 @@ if (-not (Test-Path $buildDir)) {
 }
 
 Write-Host "Building for Windows..."
-go build -o $buildDir/main.exe main.go
+go build -o $buildDir/max2tg-windows-amd64.exe main.go
 
 Write-Host "Building for Linux..."
-$env:GOOS="linux"; $env:GOARCH="amd64"; go build -o $buildDir/main main.go
+$env:GOOS="linux"; $env:GOARCH="amd64"; go build -o $buildDir/max2tg-linux-amd64 main.go
 
 Remove-Item Env:GOOS -ErrorAction SilentlyContinue
 Remove-Item Env:GOARCH -ErrorAction SilentlyContinue
